265b2a943f8b6ae943b6e1ca9df8533be43575ce
Lihe Chen <lihe.chen@gmail.com>
2020-08-22 19:19:19 -0500

    initial implementation

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,75 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let workspaceConfiguration = vscode.workspace.getConfiguration();
+const THEME = "workbench.colorTheme";
+let themeList: String[] = [];
+let curIndex: number = 0;
+let minutes: number = 0;
+let curTimer: any = null;
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "theme-tomato" is now active!');
+	loadSettings();
+	console.log('Congratulations, "theme-tomato" is now active!');
+	console.log('minutes', minutes);
+	console.log('themes', JSON.stringify(themeList));
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('theme-tomato.helloWorld', () => {
+	let start = vscode.commands.registerCommand('theme-tomato.start', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from theme-tomato!');
+		vscode.window.showInformationMessage('theme-tomato started!');
+		console.log("started", curIndex, "@", now());
+		curTimer = setInterval(() => {
+			setTheme(themeList[curIndex]);
+			curIndex = (curIndex + 1) % themeList.length;
+		}, 1000 * 60 * minutes);
 	});
+	context.subscriptions.push(start);
 
-	context.subscriptions.push(disposable);
+	let stop = vscode.commands.registerCommand('theme-tomato.stop', () => {
+		vscode.window.showInformationMessage('theme-tomato stopped!');
+		cleanUp();
+		console.log("stopped", curIndex, "@", now());
+
+	});
+	context.subscriptions.push(stop);
+
 }
 
+function loadSettings() {
+	const config = vscode.workspace.getConfiguration("theme.tomato");
+	minutes = config.minutes || 10;
+	themeList = config.themes || ['Visual Studio Dark', 'Visual Studio Light'];
+
+}
+
+function setTheme(themeName: String) {
+	console.log("Setting theme", themeName, "@", now());
+	workspaceConfiguration.update(THEME, themeName, true);
+	//reload
+	workspaceConfiguration = vscode.workspace.getConfiguration();
+}
+
+function cleanUp() {
+	if (curTimer === null) {
+		return;
+	}
+	clearInterval(curTimer);
+	curTimer = null;
+}
+
+function now(): String {
+	return (new Date()).toISOString();
+}
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	cleanUp();
+}
